764ad3e2ff8743123110c9351087e876652540d1
Carlos <ing.cfabregas@gmail.com>
2019-09-25 10:08:32 -0700

    First Commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+// import { Routes, RouterModule } from "@angular/router";
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { LoginComponent } from './login/login.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graphics1Component } from './pages/graphics1/graphics1.component';
+// import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+
+// const appRoutes:Routes=[
+
+//      { path: 'dashboard', component:DashboardComponent},     
+//      { path: 'login', component:LoginComponent},
+//      { path: 'register', component:LoginComponent},
+//      { path: 'progress', component:ProgressComponent},
+//      { path: 'graphics1', component:Graphics1Component},
+//      { path: '', redirectTo:'/dashboard', pathMatch:'full'},
+//      { path: '**',  component:NopagefoundComponent}
+// ];
+
+// RouterModule
